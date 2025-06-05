8f52fc99fb5c6b04279a79f02a8cb07b9199bf0b
George Mimoglou <mimoglou22@gmail.com>
2025-06-05 21:45:49 +0300

    changed app

--- app.py
+++ app.py
@@ -9,27 +9,23 @@ from resources.tone import blp as ToneBlueprint
 
 load_dotenv()
 
-def create_app():
-    """
-    Create and configure the Flask application.
-    """
-    app = Flask(__name__)
-    app.config["API_TITLE"] = "Tone Detection API"
-    app.config["API_VERSION"] = "v1"
-    app.config["OPENAPI_VERSION"] = "3.0.3"
-    app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config[
-        "OPENAPI_SWAGGER_UI_URL"
-    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-    api = Api(app)
+app = Flask(__name__)
 
+app.config["API_TITLE"] = "Tone Detection API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config[
+    "OPENAPI_SWAGGER_UI_URL"
+] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-    api.register_blueprint(ToneBlueprint)
+api = Api(app)
 
-    return app
+api.register_blueprint(ToneBlueprint)
 
 
-app = create_app()
-app.run(port=8080, debug=False)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
